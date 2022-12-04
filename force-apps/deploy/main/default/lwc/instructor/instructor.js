10c0125b828d51924314b3e5eafc8929eae27ad7
Andres Perez <andresperezf@yahoo.com>
2022-12-04 12:17:35 -0500

    Displays QR code from static resources

--- force-apps/deploy/main/default/lwc/instructor/instructor.js
+++ force-apps/deploy/main/default/lwc/instructor/instructor.js
@@ -1,3 +1,6 @@
 import { LightningElement } from "lwc";
+import QR from "@salesforce/resourceUrl/QR";
 
-export default class Instructor extends LightningElement {}
+export default class Instructor extends LightningElement {
+	QR = QR;
+}
