6cc07804846d52a958472be6ea63531a21d10694
Andres Perez <andresperezf@yahoo.com>
2022-12-04 17:06:00 -0800

    Images work on browser and phone

--- force-apps/deploy/main/default/lwc/instructor/instructor.js
+++ force-apps/deploy/main/default/lwc/instructor/instructor.js
@@ -1,6 +1,3 @@
 import { LightningElement } from "lwc";
-import QR from "@salesforce/resourceUrl";
 
-export default class Instructor extends LightningElement {
-	QR = `${QR}/QR`;
-}
+export default class Instructor extends LightningElement {}
