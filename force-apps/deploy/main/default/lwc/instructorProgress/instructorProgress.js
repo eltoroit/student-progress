13440ac25c759bda784edecc635f50da335fa311
Andres Perez <andresperezf@yahoo.com>
2022-12-07 06:20:34 -0800

    Displays chart (vertical)

--- force-apps/deploy/main/default/lwc/instructorProgress/instructorProgress.js
+++ force-apps/deploy/main/default/lwc/instructorProgress/instructorProgress.js
@@ -1,30 +1,44 @@
+// Chart - Review this...
+// https://salesforcelabs.github.io/LightningWebChartJS/docs/api/chart.html
+
 import Utils from "c/utils";
+// import { refreshApex } from "@salesforce/apex";
 import { api, LightningElement, wire } from "lwc";
-import { refreshApex } from "@salesforce/apex";
+import chartjs from "@salesforce/resourceUrl/Chartjs";
+import { loadScript } from "lightning/platformResourceLoader";
 import getClassReport from "@salesforce/apex/Instructor.getClassReport";
 
 export default class ClassProgress extends LightningElement {
 	@api selectedCourseDeliveryId = "";
 
-	table = [];
 	students = [];
 	exercises = [];
 	loading = false;
-
+	tableAllData = [];
 	wiredGetClassReport = null;
+
+	_ctxChart = null;
+	_chartjsInitialized = false;
+
 	@wire(getClassReport, { CxD: "$selectedCourseDeliveryId" })
 	wired_getClassReport(result) {
 		this.wiredGetClassReport = result;
-		debugger;
 		let { data, error } = result;
 		if (data) {
 			const mapStudents = {};
 			this.exercises = data.EXERCISES.map((ex) => ({ Id: ex.Id, Name: ex.Name }));
 			this.students = data.STUDENTS.map((student) => {
-				const output = { Id: student.Id, Name: student.Name, Points: 0, mExS: {} };
+				const output = {
+					Id: student.Id,
+					Name: student.Name,
+					IsInstructor: student.IsInstructor__c,
+					Points: 0,
+					mExS: {}
+				};
 				mapStudents[student.Id] = output;
 				return output;
 			});
+			// Calculate points
 			data.EXERCISES.forEach((ex) => {
 				let points = this.students.length;
 				ex.Exercises_X_Students__r.forEach((ExS, index) => {
@@ -45,11 +59,13 @@ export default class ClassProgress extends LightningElement {
 					student.mExS[ExS.Exercise__c] = newExS;
 				});
 			});
-			this.table = this.students.map((student) => {
+			// Build the output data
+			this.tableAllData = this.students.map((student) => {
 				const output = {
 					StudentId: student.Id,
 					Name: student.Name,
-					Points: student.Points
+					Points: student.Points,
+					IsInstructor: student.IsInstructor
 				};
 				this.exercises.forEach((ex, index) => {
 					const ExS = student.mExS[ex.Id];
@@ -66,8 +82,10 @@ export default class ClassProgress extends LightningElement {
 				});
 				return output;
 			});
+			// Only the students
+			this.tableAllData = this.tableAllData.filter((row) => !row.IsInstructor);
+			this.makeChart();
 			this.loading = false;
-			debugger;
 		} else if (error) {
 			Utils.showNotification(this, {
 				title: "Error (Instructor)",
@@ -77,5 +95,51 @@ export default class ClassProgress extends LightningElement {
 			console.log(error);
 			this.loading = false;
 		}
+	}
+
+	renderedCallback() {
+		if (this._chartjsInitialized) {
+			return;
+		}
+		this._chartjsInitialized = true;
+		loadScript(this, chartjs)
+			.then((...data) => {
+				console.log(data);
+				debugger;
+			})
+			.catch((error) => {
+				console.log(error);
+				debugger;
+			});
+		this._ctxChart = this.template.querySelector("canvas.certBarChart").getContext("2d");
+	}
+
+	makeChart() {
+		// Make chart data
+		this.tableAllData = this.tableAllData.sort((a, b) => (a.Points < b.Points ? -1 : 1));
+		const labels = this.tableAllData.map((row) => row.Name);
+		const data = this.tableAllData.map((row) => row.Points);
+		const options = {
+			type: "bar",
+			data: {
+				labels,
+				datasets: [
+					{
+						backgroundColor: "#639dff",
+						data
+					}
+				]
+			},
+			options: {
+				responsive: true,
+				legend: { display: false },
+				title: { display: false },
+				animation: {
+					animateScale: true
+				}
+			}
+		};
+		// eslint-disable-next-line no-new
+		new window.Chart(this._ctxChart, options);
 	}
 }
