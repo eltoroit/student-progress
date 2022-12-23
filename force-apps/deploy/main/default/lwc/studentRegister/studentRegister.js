729d20a24f35e49125fcc368b5184dada7797ef5
Andres Perez <andresperezf@yahoo.com>
2022-12-23 14:18:17 -0500

    Work on studentRegister

--- force-apps/deploy/main/default/lwc/studentRegister/studentRegister.js
+++ force-apps/deploy/main/default/lwc/studentRegister/studentRegister.js
@@ -1,11 +1,6 @@
 import Utils from "c/utils";
 import { LightningElement } from "lwc";
-// import { refreshApex } from "@salesforce/apex";
 import LightningPrompt from "lightning/prompt";
-// import registerStudent from "@salesforce/apex/Student.registerStudent";
-// import getActiveDeliveries from "@salesforce/apex/Student.getActiveDeliveries";
-// import validateRegistration from "@salesforce/apex/Student.validateRegistration";
-// import getStudentsForDelivery from "@salesforce/apex/Student.getStudentsForDelivery";
 
 export default class StudentRegister extends LightningElement {
 	// timer = null;
@@ -105,96 +100,96 @@ export default class StudentRegister extends LightningElement {
 	// 	}
 	// }
 
-	onDeliveryChange(event) {
-		this.loading = true;
-		const Id = event.target.value;
-		const option = this.deliveries.find((delivery) => delivery.value === Id);
-		this.delivery = option.delivery;
-		Utils.setCookie({ key: "deliveryId", value: this.delivery.Id });
-	}
+	// onDeliveryChange(event) {
+	// 	this.loading = true;
+	// 	const Id = event.target.value;
+	// 	const option = this.deliveries.find((delivery) => delivery.value === Id);
+	// 	this.delivery = option.delivery;
+	// 	Utils.setCookie({ key: "deliveryId", value: this.delivery.Id });
+	// }
 
-	onStudentChange(event) {
-		const Id = event.target.value;
-		const option = this.students.find((student) => student.value === Id);
-		if (option.student) {
-			this.student = option.student;
-			Utils.setCookie({ key: "studentId", value: this.student.Id });
-		} else {
-			this.student = { Id: option.value };
-		}
-	}
+	// onStudentChange(event) {
+	// 	const Id = event.target.value;
+	// 	const option = this.students.find((student) => student.value === Id);
+	// 	if (option.student) {
+	// 		this.student = option.student;
+	// 		Utils.setCookie({ key: "studentId", value: this.student.Id });
+	// 	} else {
+	// 		this.student = { Id: option.value };
+	// 	}
+	// }
 
-	onRegisterClick() {
-		LightningPrompt.open({
-			label: "Registration", // this is the header text
-			message: "What's your name?",
-			variant: "header",
-			theme: "inverse",
-			defaultValue: ""
-		}).then((studentName) => {
-			if (studentName) {
-				registerStudent({ deliveryId: this.delivery.Id, studentName })
-					.then((student) => {
-						refreshApex(this.wiredStudents);
-						this.student = student;
-						Utils.setCookie({ key: "studentId", value: this.student.Id });
-					})
-					.catch((err) => {
-						Utils.logger.log(err);
-						debugger;
-					});
-			}
-		});
-	}
+	// onRegisterClick() {
+	// 	LightningPrompt.open({
+	// 		label: "Registration", // this is the header text
+	// 		message: "What's your name?",
+	// 		variant: "header",
+	// 		theme: "inverse",
+	// 		defaultValue: ""
+	// 	}).then((studentName) => {
+	// 		if (studentName) {
+	// 			registerStudent({ deliveryId: this.delivery.Id, studentName })
+	// 				.then((student) => {
+	// 					refreshApex(this.wiredStudents);
+	// 					this.student = student;
+	// 					Utils.setCookie({ key: "studentId", value: this.student.Id });
+	// 				})
+	// 				.catch((err) => {
+	// 					Utils.logger.log(err);
+	// 					debugger;
+	// 				});
+	// 		}
+	// 	});
+	// }
 
-	onNextClick() {
-		this.dispatchEvent(
-			new CustomEvent("next", {
-				detail: {
-					deliveryId: this.delivery.Id,
-					studentId: this.student.Id
-				}
-			})
-		);
-	}
+	// onNextClick() {
+	// 	this.dispatchEvent(
+	// 		new CustomEvent("next", {
+	// 			detail: {
+	// 				deliveryId: this.delivery.Id,
+	// 				studentId: this.student.Id
+	// 			}
+	// 		})
+	// 	);
+	// }
 
-	onRefreshClick() {
-		this.loading = false;
-		this.forceRefresh++;
-	}
+	// onRefreshClick() {
+	// 	this.loading = false;
+	// 	this.forceRefresh++;
+	// }
 
-	async validateRegistrationJS() {
-		// clearInterval(this.timer);
-		// return new Promise((resolve, reject) => {
-		// 	await Utils.validateStudentRegistration({ dataManager: this.dataManager, deliveryId: this.deliveryId, studentId: this.studentId });
-		// 		.then((data) => {
-		// 			this.student = data.student;
-		// 			this.delivery = data.delivery;
-		// 			Utils.setCookie({ key: "studentId", value: this.student.Id });
-		// 			Utils.setCookie({ key: "deliveryId", value: this.delivery.Id });
-		// 			resolve();
-		// 		})
-		// 		.catch((err) => {
-		// 			this.student = {};
-		// 			this.delivery = {};
-		// 			Utils.deleteCookie({ key: "studentId" });
-		// 			Utils.deleteCookie({ key: "deliveryId" });
+	// async validateRegistrationJS() {
+	// 	// clearInterval(this.timer);
+	// 	// return new Promise((resolve, reject) => {
+	// 	// 	await Utils.validateStudentRegistration({ dataManager: this.dataManager, deliveryId: this.deliveryId, studentId: this.studentId });
+	// 	// 		.then((data) => {
+	// 	// 			this.student = data.student;
+	// 	// 			this.delivery = data.delivery;
+	// 	// 			Utils.setCookie({ key: "studentId", value: this.student.Id });
+	// 	// 			Utils.setCookie({ key: "deliveryId", value: this.delivery.Id });
+	// 	// 			resolve();
+	// 	// 		})
+	// 	// 		.catch((err) => {
+	// 	// 			this.student = {};
+	// 	// 			this.delivery = {};
+	// 	// 			Utils.deleteCookie({ key: "studentId" });
+	// 	// 			Utils.deleteCookie({ key: "deliveryId" });
 
-		// 			// this.timer = setInterval(() => {
-		// 			Utils.logger.log("***Interval deliveries");
-		// 			if (this.delivery.Id === "") {
-		// 				Utils.logger.log(`REFRESH ${this.delivery.Id}`);
-		// 				this.onRefreshClick();
-		// 			} else {
-		// 				Utils.logger.log(`CLEAR ${this.delivery.Id}`);
-		// 				clearInterval(this.timer);
-		// 			}
-		// 			// }, 5e3);
+	// 	// 			// this.timer = setInterval(() => {
+	// 	// 			Utils.logger.log("***Interval deliveries");
+	// 	// 			if (this.delivery.Id === "") {
+	// 	// 				Utils.logger.log(`REFRESH ${this.delivery.Id}`);
+	// 	// 				this.onRefreshClick();
+	// 	// 			} else {
+	// 	// 				Utils.logger.log(`CLEAR ${this.delivery.Id}`);
+	// 	// 				clearInterval(this.timer);
+	// 	// 			}
+	// 	// 			// }, 5e3);
 
-		// 			reject(err);
-		// 		});
-		// });
-	}
+	// 	// 			reject(err);
+	// 	// 		});
+	// 	// });
+	// }
 
 	readCookies() {
 		this.students.currentId = Utils.getCookie({ key: "studentId" });
