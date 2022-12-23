e44ac27cb4c99906299212636a35fbfabea924c2
Andres Perez <andresperezf@yahoo.com>
2022-12-23 14:16:53 -0500

    Hide chart until we get good data

--- force-apps/deploy/main/default/lwc/instructorDeliveryProgress/instructorDeliveryProgress.js
+++ force-apps/deploy/main/default/lwc/instructorDeliveryProgress/instructorDeliveryProgress.js
@@ -2,6 +2,7 @@
 // https://salesforcelabs.github.io/LightningWebChartJS/docs/api/chart.html
 // https://github.com/trailheadapps/lwc-recipes/blob/main/force-app/main/default/lwc/libsChartjs/libsChartjs.html
 
+import Utils from "c/utils";
 import { api, LightningElement } from "lwc";
 import ChartJS from "@salesforce/resourceUrl/chartjs_v280";
 import { loadScript } from "lightning/platformResourceLoader";
@@ -12,7 +13,12 @@ export default class InstructorDeliveryProgress extends LightningElement {
 	_chart = null;
 	_ctxChart = null;
 	_chartErrors = [];
-	_chartjsInitialized = false;
+
+	get ui() {
+		const ui = {};
+		ui.pnlCard = this.deliveryData?.length > 0;
+		return ui;
+	}
 
 	@api
 	get deliveryData() {
@@ -25,63 +31,62 @@ export default class InstructorDeliveryProgress extends LightningElement {
 		}
 	}
 
-	renderedCallback() {
-		if (this._chartjsInitialized) {
-			return;
-		}
-		this._chartjsInitialized = true;
-		loadScript(this, ChartJS)
-			.then(() => {
+	async makeChart() {
+		if (!this._ctxChart) {
+			try {
+				await loadScript(this, ChartJS);
 				const canvas = document.createElement("canvas");
 				this.template.querySelector("div.chart").appendChild(canvas);
 				this._ctxChart = canvas.getContext("2d");
-			})
-			.catch((error) => {
-				this.error = error;
-			});
-	}
-
-	makeChart() {
-		// Make chart data
-		const labels = this.deliveryData.map((row) => row.Name);
-		const data = this.deliveryData.map((row) => row.Points);
-		const config = {
-			type: "horizontalBar",
-			data: {
-				labels,
-				datasets: [
-					{
-						backgroundColor: "#639dff",
-						label: "Students",
-						data
-					}
-				]
-			},
-			options: {
-				responsive: true,
-				legend: { display: false },
-				title: { display: false },
-				animation: {
-					animateScale: true
-				}
-			}
-		};
-		try {
-			this._chart = new window.Chart(this._ctxChart, config);
-			this._chartErrors = [];
-		} catch (ex) {
-			this._chartErrors.push(ex);
-			if (this._chartErrors.length > 5) {
-				this._chartErrors.forEach((error) => {
-					Utils.logger.log(error.message);
-				});
-				// eslint-disable-next-line
-				alert(`Too many errors creating the chart have occurred. You will need to refresh the page!`);
+			} catch (ex) {
+				Utils.logger.error(ex);
+				// eslint-disable-next-line no-alert
+				alert("Can't initialize chart");
 				debugger;
-			} else {
-				setTimeout(() => {
-					this.makeChart();
-				}, 1e3);
+			}
+		}
+
+		if (this._ctxChart) {
+			// Make chart data
+			const labels = this.deliveryData.map((row) => row.Name);
+			const data = this.deliveryData.map((row) => row.Points);
+			try {
+				this._chart = new window.Chart(this._ctxChart, {
+					type: "horizontalBar",
+					data: {
+						labels,
+						datasets: [
+							{
+								backgroundColor: "#639dff",
+								label: "Students",
+								data
+							}
+						]
+					},
+					options: {
+						responsive: true,
+						legend: { display: false },
+						title: { display: false },
+						animation: {
+							animateScale: true
+						}
+					}
+				});
+				this._chartErrors = [];
+			} catch (ex) {
+				this._chartErrors.push(ex);
+				if (this._chartErrors.length > 5) {
+					this._chartErrors.forEach((error) => {
+						Utils.logger.log(error.message);
+					});
+					// eslint-disable-next-line
+					alert(`Too many errors creating the chart have occurred. You will need to refresh the page!`);
+					debugger;
+				} else {
+					setTimeout(() => {
+						this.makeChart();
+					}, 1e3);
+				}
 			}
 		}
 	}
